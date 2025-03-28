ffac212f095b8b70ea7c76e919c37b7149a90e2b
= <jp05451@yahoo.com.tw>
2025-03-28 11:34:06 +0800

    更新 yoloConverter.sh 腳本以簡化參數，並新增對訓練、驗證和測試數據集的支持

--- yoloConverter.sh
+++ yoloConverter.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <input_directory> <split> <output_file>"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <input_directory>"
     exit 1
 fi
 
 # Assign arguments to variables
 input_directory=$1
-split=$2
-output_file=$3
+# split=$2
+# output_file=$3
 
-label-studio-converter import yolo -i $input_directory/$split -o $output_file --image-root-url "/data/local-files/?d=$input_directory/$split/images"
+label-studio-converter import yolo -i $input_directory/train -o train.json --image-root-url "/data/local-files/?d=$input_directory/train/images"
+
+label-studio-converter import yolo -i $input_directory/train -o valid.json --image-root-url "/data/local-files/?d=$input_directory/valid/images"
+
+label-studio-converter import yolo -i $input_directory/train -o test.json --image-root-url "/data/local-files/?d=$input_directory/test/images"
