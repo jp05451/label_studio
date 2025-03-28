5a2d8bc5ba4ae131b4c4957c2c65e5c179ef7344
= <jp05451@yahoo.com.tw>
2025-03-28 11:42:39 +0800

    update file

--- DownloadDatasets.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from roboflow import Roboflow
-
-os.chdir("datasets")
-rf = Roboflow(api_key="7MDUg9iT30ZHZKq4cOjx")
-project = rf.workspace("hti02").project("ebike2.0-ednob")
-version = project.version(3)
-dataset = version.download("yolov8")
